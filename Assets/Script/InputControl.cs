3a5ba73c23ebcd7a16412551a43921a475dab0a1
MarcoKuo0132 <m0935008715@gmail.com>
2020-05-11 14:53:28 +0800

    update

---
+++ Assets/Script/InputControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("press key A");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("hold key A");
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            print("release key A");
+        }
+        if (Input.GetAxis("Run")!=0)
+        {
+            print("control Run axis value=" + Input.GetAxis("Run"));
+        }
+    }
+}
---
+++ Assets/Script/MoveB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveB : MonoBehaviour
+{
+    public float MoveSpeed = 1;
+    public float RotateSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(MoveSpeed * Time.deltaTime, 0, 0, Space.World);
+        transform.Rotate(0, 0, RotateSpeed * Time.deltaTime);
+    }
+}
